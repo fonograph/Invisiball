7d239d551a9218a06c87332567c8354e8b5772f5
David Fono <fono@mobilefono.com>
2015-10-09 13:06:09 -0400

    initial import

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,250 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game : MonoBehaviour {
+
+	public enum Phase { Connecting, Waiting, Playing };
+
+	public Player playerPrefab;
+	public Text scoreText1;
+	public Text scoreText2;
+	public List<GameObject> controllerObjs;
+
+	public Color TeamColor1;
+	public Color TeamColor2;
+	public Color CycleColor;
+	public Color BallColor;
+	public Color ScoreColor;
+	public AudioClip CatchSound;
+	public AudioClip FumbleSound;
+	public AudioClip PassSound;
+	public AudioClip ScoreSound;
+	public float accelTolerance;
+	public float gyroTolerance;
+	public float cycleStartLength;
+	public float cycleIncreaseLength;
+	public float cyclePassLength;
+	public float catchMargin;
+	public float passMargin;
+	public float passLength;
+	public bool suppressFumble;
+
+	public static Game Instance;
+
+	private AudioSource audioSource;
+
+	private List<Player> players;
+	private Dictionary<int, int> scores;
+	private Dictionary<int, Text> scoreTexts;
+
+	private Phase phase;
+
+	private Player holdingPlayer;
+	private List<Player> playerCycle;
+	private int playerCycleIdx;
+	private int playerCycleCount;
+
+	private IEnumerator ballCycleRoutine;
+	private IEnumerator scoreRoutine;
+
+	public Player HoldingPlayer {
+		get { return holdingPlayer; }
+	}
+
+	void Awake() {
+		Instance = this;
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Start() {
+		players = new List<Player>();
+
+		scores = new Dictionary<int, int>();
+		scores[1] = 0;
+		scores[2] = 0;
+
+		scoreTexts = new Dictionary<int, Text>();
+		scoreTexts[1] = scoreText1;
+		scoreTexts[2] = scoreText2;
+
+		scoreText1.text = "0";
+		scoreText2.text = "0";
+
+		int count = UniMoveController.GetNumConnected();
+		Debug.Log("Controllers connected: " + count);
+
+		for (int i = 0; i < count; i++)
+		{
+			Player player = Instantiate(playerPrefab);
+			player.FumbleEvent += OnPlayerFumble;
+			player.CatchEvent += OnPlayerCatch;
+			player.PassEvent += OnPlayerPass;
+			players.Add(player);
+		}
+
+		phase = Phase.Connecting;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ( phase == Phase.Connecting ) {
+			bool allConnected = true;
+			for ( int i=0; i<players.Count; i++ ) {
+				if ( players[i].controller == null ) {
+					UniMoveController controller = players[i].gameObject.AddComponent<UniMoveController>();
+					if ( controller.Init(i) ) {
+						controller.SetLED(Color.white);
+						controller.InitOrientation();
+						controller.ResetOrientation();
+						players[i].Init(controller, controllerObjs[i]);
+					} else {
+						Destroy(controller);
+						allConnected = false;
+					}
+				} 
+			}
+			if ( allConnected ) {
+				phase = Phase.Waiting;
+
+				int team = 0;
+				foreach ( Player player in players ) {
+					player.SetTeam( team%2 + 1 );
+					team++;
+				}
+			}
+		}
+
+		else if ( phase == Phase.Waiting ) {
+			if ( Input.GetKeyDown(KeyCode.Space) ) {
+				StartGame();
+			}
+		}
+	}
+
+	void StartGame() {
+		foreach ( Player player in players ) {
+			player.inGame = true;
+		}
+
+		playerCycle = GetRandomizedPlayers();
+		
+		playerCycleIdx = 0;
+		playerCycleCount = 0;
+		CycleBall(false, null);
+	}
+
+	void OnPlayerFumble(Player player) {
+		StopCoroutine(scoreRoutine);
+
+		// start ball cycle with randomized order
+
+		playerCycle = GetRandomizedPlayers();
+		playerCycle.Remove(holdingPlayer);
+
+		playerCycleIdx = 0;
+		playerCycleCount = 0;
+		CycleBall(false, null);
+
+		holdingPlayer = null;
+
+		audioSource.PlayOneShot(FumbleSound);
+	}
+
+	void OnPlayerPass(Player player) {
+		StopCoroutine(scoreRoutine);
+
+		// figure out who's closest in orientation, and start a cycle with that player
+		Player closest = null;
+		foreach ( Player p in players ) {
+			if ( p != holdingPlayer ) {
+				if ( closest == null || holdingPlayer.GetOrientationDifference(p) < holdingPlayer.GetOrientationDifference(closest) ) {
+					closest = p;
+				}
+			}
+		}
+
+		playerCycle = GetRandomizedPlayers();
+		playerCycle.Remove(holdingPlayer);
+
+		playerCycle.Remove(closest);
+		playerCycle.Insert(0, closest);
+
+		playerCycleIdx = 0; 
+		playerCycleCount = 0;
+		CycleBall(false, cyclePassLength);
+
+		holdingPlayer = null;
+
+		audioSource.PlayOneShot(PassSound);
+	}
+
+	void OnPlayerCatch(Player player) {
+		StopCoroutine(ballCycleRoutine);
+		holdingPlayer = player;
+
+		Score(0);
+
+		audioSource.PlayOneShot(CatchSound);
+	}
+
+	void CycleBall(bool advance, float? overrideLength) {
+		if ( advance ) {
+			Player playerOff = playerCycle[playerCycleIdx];
+			playerOff.CycleBallOff();
+
+			playerCycleIdx++;
+			if ( playerCycleIdx == playerCycle.Count ) {
+				playerCycleIdx = 0;
+				playerCycleCount++;
+			}
+		}
+
+		Player playerOn = playerCycle[playerCycleIdx];
+		playerOn.CycleBallOn();
+
+		float? length = overrideLength ?? cycleStartLength + playerCycleCount*cycleIncreaseLength;
+		ballCycleRoutine = WaitAndCycleBall((float)length);
+		StartCoroutine(ballCycleRoutine);
+	}
+
+	IEnumerator WaitAndCycleBall(float seconds) {
+		yield return new WaitForSeconds(seconds);
+		CycleBall(true, null);
+	}
+
+	void Score(int points) {
+		scores[holdingPlayer.team] += points;
+		scoreTexts[holdingPlayer.team].text = scores[holdingPlayer.team].ToString();
+
+		if ( points > 0 ) {
+			holdingPlayer.Score(points);
+			audioSource.PlayOneShot(ScoreSound);
+		}
+
+		scoreRoutine = WaitAndScore(2);
+		StartCoroutine(scoreRoutine);
+	}
+
+	IEnumerator WaitAndScore(float seconds) {
+		yield return new WaitForSeconds(seconds);
+		Score(1);
+	}
+
+
+	List<Player> GetRandomizedPlayers() {
+		List<Player> list = new List<Player>(players);
+		
+		for (int i = list.Count; i > 1; i--) {
+			int pos = Random.Range(0, i-1);
+			var x = list[i - 1];
+			list[i - 1] = list[pos];
+			list[pos] = x;
+		}
+
+		return list;
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,213 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public GameObject moveControllerPrefab;
+
+	public int team;
+	public bool inGame;
+	public UniMoveController controller;
+	public GameObject controllerObj;
+	private Color defaultColor;
+	private bool hasBall;
+	private DateTime hasBallStart;
+	private bool hasCycle;
+	private bool readyToCatch;
+	private bool startedPass;
+	private bool startedFumble;
+	private Vector3 lastAccel = Vector3.zero;
+	private Vector3 startOrientation;
+
+
+	public event Action<Player> FumbleEvent;
+	public event Action<Player> PassEvent;
+	public event Action<Player> CatchEvent;
+
+	void Awake() {
+	}
+
+	void Start() {
+	}
+
+	public void Init(UniMoveController c, GameObject co) {
+		controller = c;
+		controllerObj = co;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ( controllerObj != null )
+			controllerObj.transform.localRotation = controller.Orientation;
+
+		if ( !inGame ) 
+			return;
+	
+		// CATCHING
+		// you need to press the trigger WHILE you have the cycle
+		if ( hasCycle ) {
+			if ( controller.Trigger == 0 ) {
+				readyToCatch = true;
+			}
+			if ( readyToCatch && controller.Trigger > 0.5 ) {
+				CatchBall();
+			}
+		}
+
+		// PASSING
+		// you let go of the trigger, and then you have an amount of time to position yourself, and the pass fires
+		if ( hasBall && !startedPass && controller.Trigger == 0 ) {
+			Debug.Log ("passed");
+			Invoke("PassBall", Game.Instance.passLength);
+			CancelInvoke("FumbleBall");
+			startedPass = true;
+		}
+
+		// FUMBLING
+		// a jump in acceleration, or movement away from starting orientation
+		float accelTolerance = Game.Instance.accelTolerance;
+		float gyroTolerance = Game.Instance.gyroTolerance;
+		if ( hasBall && !startedPass && !startedFumble && DateTime.Now.Subtract(hasBallStart).TotalSeconds > Game.Instance.catchMargin ) { // allow a split second after receiving before fumbling is possible
+//			if ( Mathf.Abs(controller.Acceleration.x-lastAccel.x)>accelTolerance || Mathf.Abs(controller.Acceleration.y-lastAccel.y)>accelTolerance || Mathf.Abs(controller.Acceleration.z-lastAccel.z)>accelTolerance ) {
+
+			Vector3 orientationDiff = normalizedOrientationDifference(normalizedOrientation(controller.Orientation.eulerAngles), startOrientation);
+//			Debug.Log (orientationDiff);
+//			Debug.Log (orientationDiff.magnitude);
+
+			bool fumbled = false;
+
+			if ( (controller.Acceleration-lastAccel).magnitude > accelTolerance ) {
+				if ( !Game.Instance.suppressFumble )
+					fumbled = true;
+				Debug.Log("Fumbled from acceleration");
+			}
+			else if ( orientationDiff.magnitude > gyroTolerance ) {
+				if ( !Game.Instance.suppressFumble )
+					fumbled = true;
+				Debug.Log("Fumbled from orientation");
+			}
+
+			if ( fumbled ) {
+				Invoke("FumbleBall", Game.Instance.passMargin); // we delay the fumble, because if a pass begins in this time we'll call it a pass instead
+				startedFumble = true;
+			}
+
+		}
+
+		// DEBUG
+
+//		if ( hasBall ) {
+//			Debug.Log(controller.Orientation.eulerAngles);
+//			Debug.Log((controller.Orientation.eulerAngles - startOrientation));
+//			Debug.Log((controller.Orientation.eulerAngles - startOrientation).magnitude);
+//		}
+//		if ( Game.Instance.HoldingPlayer != null && Game.Instance.HoldingPlayer != this ) {
+//			Vector2 direction = new Vector2(controller.Orientation.eulerAngles.x, controller.Orientation.eulerAngles.y);
+//			Vector2 otherDirection = new Vector2(Game.Instance.HoldingPlayer.controller.Orientation.eulerAngles.x, Game.Instance.HoldingPlayer.controller.Orientation.eulerAngles.y);
+//			//Debug.Log( (controller.Orientation.eulerAngles-Game.Instance.HoldingPlayer.controller.Orientation.eulerAngles).magnitude ) ;
+//
+//			if ( direction.x > 180 )
+//				direction.x -= 360;
+//			if ( direction.y > 180 )
+//				direction.y -= 360;
+//			if ( otherDirection.x > 180 ) 
+//				otherDirection.x -= 360;
+//			if ( otherDirection.y > 180 ) 
+//				otherDirection.y -= 360;
+
+			//Debug.Log( (direction-otherDirection).magnitude );
+//			Debug.Log (direction);
+//			Debug.Log (otherDirection);
+//		}
+
+		lastAccel = controller.Acceleration;
+	}
+
+	public void SetTeam(int theTeam) {
+		team = theTeam;
+		defaultColor = theTeam == 1 ? Game.Instance.TeamColor1 : Game.Instance.TeamColor2;
+
+		controller.SetLED(defaultColor);
+	}
+
+	// STATE
+
+	public void CycleBallOn() {
+		controller.SetLED(Game.Instance.CycleColor);
+		hasCycle = true;
+		readyToCatch = false;
+	}
+
+	public void CycleBallOff() {
+		controller.SetLED(defaultColor);
+		hasCycle = false;
+	}
+
+	public void GainBall() {
+		controller.SetLED(Game.Instance.BallColor);
+		hasBall = true;
+		hasBallStart = DateTime.Now;
+		hasCycle = false;
+		startedPass = false;
+		startedFumble = false;
+		startOrientation = normalizedOrientation(controller.Orientation.eulerAngles);
+	}
+
+	public void LoseBall() {
+		controller.SetLED(defaultColor);
+		hasBall = false;
+	}
+
+	public void Score(int points) {
+		controller.SetLED(Game.Instance.ScoreColor);
+		StartCoroutine(SetLEDDelayed(Game.Instance.BallColor, 0.2f));
+	}
+
+	// INTERNAL EVENTS
+
+	public void CatchBall() {
+		GainBall();
+		CatchEvent(this);
+	}
+
+	public void PassBall() {
+		LoseBall();
+		PassEvent(this);
+	}
+
+	public void FumbleBall() {
+		LoseBall();
+		FumbleEvent(this);
+	}
+
+	// MISC
+
+	public float GetOrientationDifference(Player player) {
+		Vector3 diff = normalizedOrientationDifference(normalizedOrientation(controller.Orientation.eulerAngles), normalizedOrientation(player.controller.Orientation.eulerAngles));
+		Vector2 diff2 = new Vector2(diff.x, diff.y);
+
+		return diff2.magnitude;
+	}
+
+	private IEnumerator SetLEDDelayed(Color color, float seconds) {
+		yield return new WaitForSeconds(seconds);
+		controller.SetLED(color);
+	}
+
+	private Vector3 normalizedOrientation(Vector3 o) {
+		Vector3 orientation = o;
+		orientation.x -= orientation.x > 180 ? 360 : 0;
+		orientation.y -= orientation.y > 180 ? 360 : 0;
+		orientation.z -= orientation.z > 180 ? 360 : 0;
+		return orientation;
+	}
+
+	private Vector3 normalizedOrientationDifference(Vector3 normalizedOrientation1, Vector3 normalizedOrientation2) {
+		Vector3 orientationDiff = normalizedOrientation1 - normalizedOrientation2;
+		orientationDiff.x += (orientationDiff.x>180) ? -360 : (orientationDiff.x<-180) ? 360 : 0;
+		orientationDiff.y += (orientationDiff.y>180) ? -360 : (orientationDiff.y<-180) ? 360 : 0;
+		orientationDiff.z += (orientationDiff.z>180) ? -360 : (orientationDiff.z<-180) ? 360 : 0;
+		return orientationDiff;
+	}
+}
